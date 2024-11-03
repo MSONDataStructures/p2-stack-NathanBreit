b78fcf6b0149710976bfb692ea2ae0e97fa53846
NathanBreit <ngbreit@icloud.com>
2024-11-03 15:13:33 -0600

    I implemented Push, Pop, Top, isEmpty, and Size. Every test is passed, and unless there is something I'm forgetting I'm done with this assignment.

--- src/main/java/LinkedStack.java
+++ src/main/java/LinkedStack.java
@@ -9,18 +9,22 @@
 public class LinkedStack<T> implements StackInterface<T> {
 
     Node<T> first;
-    int size;
+    int size = 0;
 
     /**
      * {@inheritDoc}.
      */
     @Override
     public T pop() {
+        if (first == null) return null;
+        T returnValue = first.getElement();
+        first = first.getNext();
         // TODO: Implement the pop method, which will be
         //   similar to a removeFirst method for a LinkedList
         //   (a special case of our remove method).
         //   If the list is empty you should return a null value.
-        return null;
+        size--;
+        return returnValue;
     }
 
     /**
@@ -28,11 +32,12 @@ public class LinkedStack<T> implements StackInterface<T> {
      */
     @Override
     public T top() {
+        if (first == null) return null;
+        return first.getElement();
         // TODO: Implement the top method, which will be
         //   similar to a getFirst method for a LinkedList
         //   (a special case of our get method).
         //   If the list is empty you should return a null.
-        return null;
     }
 
     /**
@@ -40,6 +45,11 @@ public class LinkedStack<T> implements StackInterface<T> {
      */
     @Override
     public void push(T elem) throws NullPointerException {
+        if (elem == null) throw new NullPointerException();
+        Node<T> newNode = new Node<>(elem);
+        newNode.setNext(first);
+        first = newNode;
+        size++;
         // TODO: Implement the push method, which will be
         //   similar to the addFirst method for a LinkedList.
         //   If elem is null you should throw an exception.
